0b7b95b85cc85a27ce34989db975d392b12d5a4d
isuru kaldera <isuru.kaldera6425@gmail.com>
2025-03-08 19:03:42 +0530

    Image & icon adding

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -56,4 +56,8 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+    implementation("com.github.ismaeldivita:chip-navigation-bar:1.4.0")
+    implementation("androidx.viewpager2:viewpager2:1.1.0")
+    implementation("com.github.bumptech.glide:glide:4.16.0")
+
 }
