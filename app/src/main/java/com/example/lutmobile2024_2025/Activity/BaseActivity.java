1d1cd270a527a782dfd91a7c804f0c2511ec52a7
isuru kaldera <isuru.kaldera6425@gmail.com>
2025-03-18 17:50:00 +0530

    viewholder_popular page

--- app/src/main/java/com/example/lutmobile2024_2025/Activity/BaseActivity.java
+++ app/src/main/java/com/example/lutmobile2024_2025/Activity/BaseActivity.java
@@ -1,4 +1,19 @@
 package com.example.lutmobile2024_2025.Activity;
 
+import android.view.Window;
+import android.view.WindowManager;
+
+import com.google.firebase.database.FirebaseDatabase;
+
 public class BaseActivity {
+
+    FirebaseDatabase database = FirebaseDatabase.getInstance();
+
+
+    Window w = getWindow();
+        w.setFlags(
+    WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
+    WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS
+        );
+
 }
--- app/src/main/java/com/example/lutmobile2024_2025/Activity/MainActivity.java
+++ app/src/main/java/com/example/lutmobile2024_2025/Activity/MainActivity.java
@@ -1,27 +1,70 @@
 package com.example.lutmobile2024_2025.Activity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.GridLayout;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.lutmobile2024_2025.Adapter.CategoryAdapter;
+import com.example.lutmobile2024_2025.Model.CategoryModel;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
+
+public class MainActivity extends BaseActivity {
+    ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        binding =AcitivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
 
+        initCategory();
+    }
 
-        FirebaseDatabase database = FirebaseDatabase.getInstance();
+    private void initCategory() {
+        DatabaseReference myref=database.getReference( "Category");
+        binding.progressBarCategory.setVisibility(View.VISIBLE);
+        ArrayList<CategoryModel> list=new ArrayList<>();
 
+        myref.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                if(snapshot.exists()){
+                    for (DataSnapshot issue: snapshot.getChildren()){
+                        list.add(issue.getValue(CategoryModel.class));
+                    }
+                    if (!list.isEmpty()){
+                        binding.recyclerViewCategory.setLayoutManager(
+                                new GridLayoutManager(MainActivity.this,4)
 
-        Window w = getWindow();
-        w.setFlags(
-                WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
-                WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS
-        );
+                        );
+
+                        RecyclerView.Adapter adapter=new CategoryAdapter(list);
+                        binding.recyclerViewCategory.setAdapter(adapter);
+
+                    }
+                    binding.progressBarCategory.setVisibility(View.GONE)
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
     }
 }
